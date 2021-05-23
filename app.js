5df51ee5ede9ee49c14e94ddeb1e1969716be84a
Tharindu Dhanushka(Shan Leo) <76651361+Shan-leo@users.noreply.github.com>
2021-05-23 18:34:33 +0530

    Chaned route handlers

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ const app = express()
 app.set('view engine', 'ejs');
 
 app.use(bodyParser.urlencoded({
-    extended: true
+  extended: true
 }));
 
 app.use(express.static("public"));
@@ -16,7 +16,7 @@ app.use(express.static("public"));
 mongoose.connect("mongodb://localhost:27017/wikiDB", {
   useNewUrlParser: true,
   useUnifiedTopology: true
-}, function(error) {
+}, function (error) {
   if (error) {
     console.log(error.message);
   } else {
@@ -25,45 +25,68 @@ mongoose.connect("mongodb://localhost:27017/wikiDB", {
 })
 
 const articleSchema = {
-    title:String,
-    content:String
+  title: String,
+  content: String
 };
 
 const Article = mongoose.model("Article", articleSchema);
 
-
-app.get("/articles", function(req, res){
-    Article.find(function(err, foundArticles){
-        if (!err){
-            res.send(foundArticles);
-        }
-        else{
-            res.send(err);
-        }      
+app.route("/articles")
+  .get(function (req, res) {
+    Article.find(function (err, foundArticles) {
+      if (!err) {
+        res.send(foundArticles);
+      } else {
+        res.send(err);
+      }
     });
-});
-
-app.post("/articles", function(req, res){
+  })
+  .post(function (req, res) {
     console.log();
     console.log();
 
     const newArticle = new Article({
-      title:req.body.title,
-      content:req.body.content
+      title: req.body.title,
+      content: req.body.content
     })
 
-    newArticle.save(function (err){
-      if (!err){
+    newArticle.save(function (err) {
+      if (!err) {
         res.send("Successfully added a new article.");
-      }else{
+      } else {
         res.send(err);
       }
     });
+  })
+  .delete(function (req, res) {
+    Article.deleteMany(function (err) {
+      if (!err) {
+        res.send("All articles deleted successfully");
+      } else {
+        res.send(err)
+      }
+    })
+  })
+
+
+
+
+app.listen(3000, function () {
+  console.log("Server started on port 3000");
 });
 
-
-
-
-app.listen(3000, function(){
-    console.log("Server started on port 3000");
-});
+/*
+[
+    {
+        "_id": "60aa2bb3c69c7d2cfc1b4b47",
+        "title": "Shan Leo",
+        "content": "Explore Mario Arends's board \"Random shit\" on Pinterest. See more ideas about bones funny, best funny pictures, nato phonetic alphabet.",
+        "__v": 0
+    },
+    {
+        "_id": "60aa32696c24b428849c82b2",
+        "title": "Imalsha Sandeepani",
+        "content": "Amazingly loving",
+        "__v": 0
+    }
+] */
